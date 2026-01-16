4fc5770e396a33010d12819e8ce6508954eb7a98
Phoevo <148871904+phoevo@users.noreply.github.com>
2026-01-16 19:38:23 +0200

    removed red bg

--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -35,7 +35,7 @@ const projects = [
 function Projects() {
   return (
     <div className=''>
-      <ScrollArea className='h-[70vh] bg-red-500'>
+      <ScrollArea className='h-[70vh]'>
         <div className='mt-5 px-4 md:px-6'>
           <div className='grid grid-cols-1 gap-6 lg:grid-cols-2 md:w-full items-stretch'>
         {projects.map((project) => (
